d83fdd37e1b11824d84bada691e7c9c19630ae16
Jontze <42588836+jontze@users.noreply.github.com>
2021-11-14 00:08:21 +0100

    feat(main): Setup basic bot that receive messages

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, _ctx: Context, _new_message: Message) {
+        println!("{:?}", _new_message);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = std::env::var("DISCORD_TOKEN").expect("Expect token in environment");
+
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .await
+        .expect("Should on creating client");
+
+    let start = client.start().await;
+    match start {
+        Ok(()) => println!("Start Cadency-rs client..."),
+        Err(start_error) => println!("Client start error: {:?}", start_error),
+    }
 }
