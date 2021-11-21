455ae8dd81275dca47676e6052f176bc53bb34f1
Jontze <42588836+jontze@users.noreply.github.com>
2021-11-21 15:55:54 +0100

    refactor(commands): Implement command trait and refactor existing commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,19 +1,36 @@
 use serenity::{
+    async_trait,
     client::Context,
     model::interactions::{
-        application_command::ApplicationCommandInteraction, InteractionResponseType,
+        application_command::{ApplicationCommand, ApplicationCommandInteraction},
+        InteractionResponseType,
     },
 };
 
+pub mod fib;
 pub mod inspire;
 pub mod ping;
+
+pub use fib::Fib;
+pub use inspire::Inspire;
+pub use ping::Ping;
+
+#[async_trait]
+pub trait Command {
+    async fn register(ctx: &Context) -> Result<ApplicationCommand, serenity::Error>;
+    async fn execute(
+        ctx: &Context,
+        command: ApplicationCommandInteraction,
+    ) -> Result<(), serenity::Error>;
+}
 
 /// Submit global slash commands to the discord api.
 /// As global commands are cached for 1 hour, the activation ca take some time.
 /// For local testing it is recommended to create commandswith a guild scope.
 pub async fn setup_commands(ctx: &Context) -> Result<(), serenity::Error> {
-    let _ping_cmd = ping::create(ctx).await?;
-    let _inspire_cmd = inspire::create(ctx).await?;
+    let _ping_cmd = Ping::register(ctx).await?;
+    let _inspire_cmd = Inspire::register(ctx).await?;
+    let _fib_cmd = Fib::register(ctx).await?;
     Ok(())
 }
 
