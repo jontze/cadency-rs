bf89f178b6208aaf21907300e8fa19a3e0818571
Jontze <42588836+jontze@users.noreply.github.com>
2022-09-15 00:05:20 +0200

    test(command): Move command macro testing to command crate

--- cadency_commands/src/lib.rs
+++ cadency_commands/src/lib.rs
@@ -29,3 +29,51 @@ mod tracks;
 pub use tracks::Tracks;
 mod urban;
 pub use urban::Urban;
+
+#[cfg(test)]
+mod test {
+    use cadency_core::CadencyCommandOption;
+
+    #[test]
+    fn impl_commandbaseline_trait_with_macro() {
+        #[derive(cadency_codegen::CommandBaseline)]
+        struct Test {
+            description: String,
+            options: Vec<CadencyCommandOption>,
+        }
+        assert!(true)
+    }
+
+    #[test]
+    fn return_lowercase_struct_name_as_name() {
+        #[derive(cadency_codegen::CommandBaseline)]
+        struct Test {
+            description: String,
+            options: Vec<CadencyCommandOption>,
+        }
+        let test = Test {
+            description: "123".to_string(),
+            options: Vec::new(),
+        };
+        let name: String = test.name();
+        assert_eq!(name, "test", "Test command name ton be lowercase {name}")
+    }
+
+    #[test]
+    fn not_return_uppercase_struct_name_as_name() {
+        #[derive(cadency_codegen::CommandBaseline)]
+        struct Test {
+            description: String,
+            options: Vec<CadencyCommandOption>,
+        }
+        let test = Test {
+            description: "123".to_string(),
+            options: Vec::new(),
+        };
+        let name: String = test.name();
+        assert_ne!(
+            name, "Test",
+            "Testing that the first char is not uppercase: {name}"
+        )
+    }
+}
