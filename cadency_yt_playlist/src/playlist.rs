7ae443862c5b332ab43f4d081ad548f4421615f4
Jontze <42588836+jontze@users.noreply.github.com>
2022-09-21 21:46:33 +0200

    refactor(yt_playlist): Move function out of trait

--- cadency_yt_playlist/src/playlist.rs
+++ cadency_yt_playlist/src/playlist.rs
@@ -4,67 +4,57 @@ use crate::{
     ytdlp::YtDlp,
 };
 
-pub struct YtPlaylist;
+pub fn fetch_playlist_songs(
+    playlist_url: String,
+) -> Result<YtDlpResponse<Vec<PlaylistItem>>, YtPlaylistError> {
+    let ytdl_output = YtDlp::new()
+        .arg("--flat-playlist".to_string())
+        .arg("-j".to_string())
+        .arg(playlist_url)
+        .execute()?;
 
-pub trait Playlist {
-    fn fetch_playlist_songs(
-        playlist_url: String,
-    ) -> Result<YtDlpResponse<Vec<PlaylistItem>>, YtPlaylistError>;
-}
-
-impl Playlist for YtPlaylist {
-    fn fetch_playlist_songs(
-        playlist_url: String,
-    ) -> Result<YtDlpResponse<Vec<PlaylistItem>>, YtPlaylistError> {
-        let ytdl_output = YtDlp::new()
-            .arg("--flat-playlist".to_string())
-            .arg("-j".to_string())
-            .arg(playlist_url)
-            .execute()?;
-
-        let mut failed_deserializing: Vec<Message> = Vec::new();
-        let deserialized_items: Vec<PlaylistItem> = String::from_utf8(ytdl_output.stdout)?
-            .split('\n')
-            .into_iter()
-            .filter_map(|string_output| {
-                if string_output.is_empty() {
-                    None
-                } else {
-                    Some(
-                        serde_json::from_slice::<PlaylistItem>(string_output.as_bytes()).map_err(
-                            |err| YtPlaylistError::Deserializing {
-                                message: err.to_string(),
-                                data: string_output.to_string(),
-                                source: err,
-                            },
-                        ),
-                    )
-                }
-            })
-            .filter_map(|parsed_item| match parsed_item {
-                Ok(valid_item) => Some(valid_item),
-                Err(des_err) => {
-                    if let YtPlaylistError::Deserializing {
-                        source: _,
-                        message,
-                        data,
-                    } = des_err
-                    {
-                        failed_deserializing.push(Message {
-                            content: message,
-                            data,
-                        });
-                    };
-                    None
-                }
-            })
-            .collect();
-
-        Ok(YtDlpResponse {
-            data: deserialized_items,
-            messages: failed_deserializing,
+    let mut failed_deserializing: Vec<Message> = Vec::new();
+    let deserialized_items: Vec<PlaylistItem> = String::from_utf8(ytdl_output.stdout)?
+        .split('\n')
+        .into_iter()
+        .filter_map(|string_output| {
+            if string_output.is_empty() {
+                None
+            } else {
+                Some(
+                    serde_json::from_slice::<PlaylistItem>(string_output.as_bytes()).map_err(
+                        |err| YtPlaylistError::Deserializing {
+                            message: err.to_string(),
+                            data: string_output.to_string(),
+                            source: err,
+                        },
+                    ),
+                )
+            }
         })
-    }
+        .filter_map(|parsed_item| match parsed_item {
+            Ok(valid_item) => Some(valid_item),
+            Err(des_err) => {
+                if let YtPlaylistError::Deserializing {
+                    source: _,
+                    message,
+                    data,
+                } = des_err
+                {
+                    failed_deserializing.push(Message {
+                        content: message,
+                        data,
+                    });
+                };
+                None
+            }
+        })
+        .collect();
+
+    Ok(YtDlpResponse {
+        data: deserialized_items,
+        messages: failed_deserializing,
+    })
 }
 
 #[cfg(test)]
@@ -73,7 +63,7 @@ mod test {
 
     #[test]
     fn should_fetch_playlist_songs() {
-        let response = YtPlaylist::fetch_playlist_songs(
+        let response = fetch_playlist_songs(
             "https://www.youtube.com/watch?v=BJ8XPi-cPkM&list=PLDfKAXSi6kUZbsoz3AcUYjy8n6hbulk4o"
                 .to_string(),
         )
